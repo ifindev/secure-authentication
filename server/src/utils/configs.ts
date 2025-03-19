b399a69ab775079a69832e2b16b1ef9b54d3ae39
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-19 09:34:31 +0700

    feat: setup configs

---
+++ server/src/utils/configs.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const configs = {
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || '',
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || '',
+  refreshTokenExpiration: 1 * 60 * 60 * 1000,
+  port: process.env.PORT || 5000,
+};
+
+export default configs;

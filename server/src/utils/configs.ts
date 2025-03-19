efc13e304631976164192ef5cf5a912126a75004
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-19 10:56:04 +0700

    feat: add nodeEnv config

--- server/src/utils/configs.ts
+++ server/src/utils/configs.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const configs = {
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || '',
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || '',
+  nodeEnv: process.env.NODE_ENV,
   refreshTokenExpiration: 1 * 60 * 60 * 1000,
   port: process.env.PORT || 5000,
 };

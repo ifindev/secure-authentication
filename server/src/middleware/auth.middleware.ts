e9ceff91137de6ec62aa9d80c940777d716250de
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-19 09:33:17 +0700

    feat: setup auth middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import status from 'http-status';
+import JWT from '../utils/jwt';
+
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token)
+    return res.status(status.UNAUTHORIZED).json({
+      error: 'No token provided',
+    });
+
+  try {
+    const payload = JWT.verifyAccessToken(token);
+    req.user = {
+      userId: payload.userId,
+    };
+  } catch (error) {
+    res.status(status.FORBIDDEN).json({
+      error: 'Invalid or expired token',
+    });
+  }
+};
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+      };
+    }
+  }
+}

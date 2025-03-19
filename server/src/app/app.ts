32b57595da4e8280d8f6109748c986f06e3c15a8
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-19 09:35:23 +0700

    feat: add app class and server

---
+++ server/src/app/app.ts
@@ -0,0 +1,41 @@
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import { errorMiddleware } from '../middleware/error.middleware';
+import authRouter from '../routes/auth.route';
+import configs from '../utils/configs';
+
+dotenv.config();
+
+export default class App {
+  private app: Express;
+  private readonly PORT: number | string;
+
+  constructor() {
+    this.app = express();
+    this.PORT = configs.port;
+
+    this.setupMiddlewares();
+    this.setupRoutes();
+    this.setupErrorHandler();
+  }
+
+  private setupMiddlewares(): void {
+    this.app.use(express.json());
+    this.app.use(cookieParser());
+  }
+
+  private setupErrorHandler(): void {
+    this.app.use(errorMiddleware);
+  }
+
+  private setupRoutes(): void {
+    this.app.use('/api/auth', authRouter);
+  }
+
+  public start(): void {
+    this.app.listen(this.PORT, () => {
+      console.log(`Server running on port ${this.PORT}`);
+    });
+  }
+}

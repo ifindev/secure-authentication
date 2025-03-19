53192b117f30ae7b4a5a3192c2701125269f5633
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-19 12:00:09 +0700

    feat: fix authenticateToken middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -2,38 +2,37 @@ import { NextFunction, Request, Response } from 'express';
 import status from 'http-status';
 import JWT from '../utils/jwt';
 
-export const authenticateToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token)
-    return res.status(status.UNAUTHORIZED).json({
-      error: 'No token provided',
-    });
+    if (!token) {
+        res.status(status.UNAUTHORIZED).json({
+            error: 'No token provided',
+        });
+        return;
+    }
 
-  try {
-    const payload = JWT.verifyAccessToken(token);
-    req.user = {
-      userId: payload.userId,
-    };
-    next();
-  } catch (error) {
-    res.status(status.FORBIDDEN).json({
-      error: 'Invalid or expired token',
-    });
-  }
+    try {
+        const payload = JWT.verifyAccessToken(token);
+        req.user = {
+            userId: payload.userId,
+        };
+        next();
+    } catch (error) {
+        res.status(status.FORBIDDEN).json({
+            error: 'Invalid or expired token',
+        });
+        return;
+    }
 };
 
 declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        userId: string;
-      };
+    namespace Express {
+        interface Request {
+            user?: {
+                userId: string;
+            };
+        }
     }
-  }
 }

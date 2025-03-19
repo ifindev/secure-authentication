dd3f19fccdd838f1916ecbab4132918a4ddc2e88
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-19 10:56:26 +0700

    feat: update delay and data access in userRepository

--- server/src/repositories/user.repository.ts
+++ server/src/repositories/user.repository.ts
@@ -1,9 +1,9 @@
-import { users } from '../db/db';
+import db from '../db/db';
 import { delay } from '../utils/time';
 
 export default class UserRepository {
   static async findUserByUsername(username: string) {
-    await delay(200); // Simulate 200ms database latency
-    return users.find((user) => user.username === username) || null;
+    await delay(100); // Simulate 200ms database latency
+    return db.users.find((user) => user.username === username) || null;
   }
 }

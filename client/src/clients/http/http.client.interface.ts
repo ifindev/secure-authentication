4249e82a3ca7934e41121f178584634c8b92c479
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-21 10:02:11 +0700

    feat(client): add IAuthHttpClient

--- client/src/clients/http/http.client.interface.ts
+++ client/src/clients/http/http.client.interface.ts
@@ -1,7 +1,13 @@
-export default interface IHttpClient {
+export interface IHttpClient {
     delete<T>(url: string, config?: object): Promise<T>;
     get<T>(url: string, config?: object): Promise<T>;
     patch<T>(url: string, data?: object, config?: object): Promise<T>;
     post<T>(url: string, data?: object, config?: object): Promise<T>;
     put<T>(url: string, data?: object, config?: object): Promise<T>;
+}
+
+
+export interface IAuthHttpClient extends IHttpClient {
+    logout(): Promise<void>;
+    refreshToken(): Promise<{accessToken: string }>;
 }

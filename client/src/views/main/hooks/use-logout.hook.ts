4dc03d5bf704083af4b7a1227eedc97a0da3a678
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-21 10:44:19 +0700

    feat(client): add logout hook

---
+++ client/src/views/main/hooks/use-logout.hook.ts
@@ -0,0 +1,32 @@
+import { useMutation } from '@tanstack/react-query';
+import { useCallback } from 'react';
+
+import authRepository from '../../../repositories/auth.repository';
+
+type UseLogoutProps = {
+    onError?: (err: Error) => void;
+    onSuccess?: () => void;
+};
+
+export default function useLogout({ onError, onSuccess }: UseLogoutProps) {
+    const { isPending, mutate, isError, error } = useMutation({
+        mutationFn: authRepository.logout,
+        onSuccess: () => {
+            onSuccess?.();
+        },
+        onError: (err) => {
+            onError?.(err);
+        },
+    });
+
+    const execute = useCallback(() => {
+        mutate();
+    }, []);
+
+    return {
+        execute,
+        isPending,
+        isError,
+        error,
+    };
+}

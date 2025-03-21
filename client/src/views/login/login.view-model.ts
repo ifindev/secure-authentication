9b850aaa43906e03d54557bf7b67591a85567d5f
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-21 10:19:31 +0700

    feat(client): update login view-model

--- client/src/views/login/login.view-model.ts
+++ client/src/views/login/login.view-model.ts
@@ -1,8 +1,9 @@
 import { useNavigate } from 'react-router-dom';
+
 import authStore from '../../stores/auth.store';
 import mainRoute from '../main/main.route';
-import useLoginForm from './hooks/use-login-form.hook';
 import useLogin from './hooks/use-login.hook';
+import useLoginForm from './hooks/use-login-form.hook';
 
 export default function useLoginViewModel() {
     // #region NAVIGATION
@@ -16,7 +17,7 @@ export default function useLoginViewModel() {
     // #region LOGIN ACTION
     const login = useLogin({
         onSuccess: (res) => {
-            authStore.actions.login(res.accessToken);
+            authStore.actions.setToken(res.accessToken);
             navigate(mainRoute.path);
         },
     });

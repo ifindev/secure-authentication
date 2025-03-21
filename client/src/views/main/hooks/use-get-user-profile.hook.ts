dc202b3edc704bda6478f072d701b98bb9c5c380
Muhammad Arifin <arifin.muhammad2610@Gmail.com>
2025-03-21 10:20:21 +0700

    feat(client): add hooks to get user & for refresh token

---
+++ client/src/views/main/hooks/use-get-user-profile.hook.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { useSnapshot } from 'valtio';
+
+import userRepository, { UserProfile } from '../../../repositories/user.repository';
+import authStore from '../../../stores/auth.store';
+
+export default function useGetUserProfile() {
+    const { accessToken } = useSnapshot(authStore.state);
+
+    return useQuery<UserProfile>({
+        enabled: !!accessToken,
+        queryKey: ['user-profile'],
+        queryFn: () => userRepository.getProfile(),
+        retry: false,
+        staleTime: 5 * 60 * 1000, // 5 minutes
+    });
+}
---
+++ client/src/views/main/hooks/use-refresh-token.hook.ts
@@ -0,0 +1,32 @@
+import { useMutation } from '@tanstack/react-query';
+import { useCallback } from 'react';
+
+import authRepository, { LoginRes } from '../../../repositories/auth.repository';
+
+type UseRefreshTokenProps = {
+    onError?: (err: Error) => void;
+    onSuccess?: (data: LoginRes) => void;
+};
+
+export default function useRefreshToken({ onError, onSuccess }: UseRefreshTokenProps) {
+    const { isPending, mutate, isError, error } = useMutation({
+        mutationFn: authRepository.refreshToken,
+        onSuccess: (data) => {
+            onSuccess?.(data);
+        },
+        onError: (err) => {
+            onError?.(err);
+        },
+    });
+
+    const execute = useCallback(() => {
+        mutate();
+    }, []);
+
+    return {
+        execute,
+        isPending,
+        isError,
+        error,
+    };
+}
